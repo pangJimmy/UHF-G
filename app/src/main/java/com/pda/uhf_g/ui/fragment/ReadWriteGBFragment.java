fb6761d76b177fcb65a083e9c5056dc8ee1a85b4
pangJimmy <1018455010@qq.com>
2022-04-19 16:25:07 +0800

    国标临时提交

--- app/src/main/java/com/pda/uhf_g/ui/fragment/ReadWriteGBFragment.java
+++ app/src/main/java/com/pda/uhf_g/ui/fragment/ReadWriteGBFragment.java
@@ -8,6 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.pda.uhf_g.R;
@@ -22,10 +26,47 @@ import butterknife.OnClick;
 
 public class ReadWriteGBFragment extends BaseFragment {
 
-    @BindView(R.id.textView_test)
-    TextView textView ;
-    @BindView(R.id.button_test)
-    Button buttonTest ;
+    @BindView(R.id.spinner_epc)
+    Spinner spinnerEPC ;
+    @BindView(R.id.radio_membank)
+    RadioGroup radioGroupMembank ;
+
+    @BindView(R.id.checkbox_filter)
+    CheckBox checkBoxFilter ;
+    @BindView(R.id.editText_start_addr)
+    EditText editTextStartAddr ;
+    @BindView(R.id.editText_len)
+    EditText editTextLen ;
+    @BindView(R.id.editText_access_password)
+    EditText editTextAccessPassword ;
+    @BindView(R.id.editText_write_data)
+    EditText editTextWriteData ;
+    @BindView(R.id.editText_read_data)
+    EditText editTextReadData ;
+    @BindView(R.id.button_read)
+    Button buttonRead ;
+    @BindView(R.id.button_write)
+    Button buttonWrite ;
+    @BindView(R.id.button_clean)
+    Button buttonClean ;
+
+    @BindView(R.id.sipnner_lock_data)
+    Spinner spinnerLockData ;
+    @BindView(R.id.sipnner_lock_type)
+    Spinner spinnerLockType ;
+    @BindView(R.id.editText_lock_password)
+    EditText editTextLockPassword ;
+    @BindView(R.id.button_lock)
+    Button buttonLock ;
+    @BindView(R.id.editText_kill_password)
+    EditText editTextKillPassword ;
+    @BindView(R.id.button_kill)
+    Button buttonKill ;
+
+    @BindView(R.id.editText_new_epc)
+    EditText editTextNewEPC ;
+    @BindView(R.id.button_modify)
+    Button buttonModify ;
 
     private boolean isRunning = false ;
     private boolean isStart = false ;
@@ -35,57 +76,15 @@ public class ReadWriteGBFragment extends BaseFragment {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            switch (msg.what) {
-                case 1:
-                    textView.setText("" + count);
-                    //在这时播放声音是错误的
-                    play() ;
-                    break;
-            }
+
         }
     } ;
 
-    long time = 0L;
-    public void play() {
-        if (System.currentTimeMillis() - time > 40) {
-            time = System.currentTimeMillis() ;
-            UtilSound.play(1, 0);
-        }
-
-    }
 
 
-    private Runnable task = new Runnable() {
-        @Override
-        public void run() {
-            if (isStart) {
-                while (isRunning) {
-                    count++ ;
-                    handler.sendEmptyMessage(1);
-
-                    try {
-                        Thread.sleep(20);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-    } ;
     private Context mainActivity;
 
 
-    @OnClick(R.id.button_test)
-    void clickTest() {
-        if (!isStart) {
-            isStart = true ;
-            isRunning = true ;
-            new Thread(task).start();
-        }else{
-            isStart = false ;
-            isRunning = false ;
-        }
-    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
